3bf36f2c226d954c5aeef84d7a23c4c10fac8ee3
azeez1776 <aboud3am81@gmail.com>
2021-09-03 17:00:47 +0300

    added express validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
 
 //desc registers users
 //route POST api/users
 //access public
 
-router.post('/', (req, res) => res.send('Created user'));
+router.post('/',[check('name', 'Please include your name')
+.not()
+.isEmpty(),
+check('email', 'Please include email')
+.isEmail(),
+check('password', 'Password must include 6 characters and more')
+.isLength({min:6})
+],
+(req, res) => {
+
+    const errors = validationResult(req);
+    
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    else{
+        return res.send('passed');
+    }
+});
 
 module.exports = router;
